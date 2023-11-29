f67a4aefcfb7c045c2e310997f6172c21cd257fb
sallalhaider <sallalhaider8@gmail.com>
2023-11-29 05:22:38 +0500

    okta development basic code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+function onAuthRequired({ history }) {
+  history.push('/login');
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security
+          issuer="https://dev-18515720.okta.com/oauth2/default"
+          client_id="0oadj34ktt4TpWFDo5d7"
+          redirect_uri={window.location.origin + '/implicit/callback'}
+          onAuthRequired={onAuthRequired}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route path="/" exact={true} component={Home} />
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+              <Route
+                path="/login"
+                render={() => (
+                  <Login baseUrl="https://dev-18515720.okta.com" />
+                )}
+              />
+              <Route path="/implicit/callback" component={LoginCallback} />
+            </div>
+          </div>
+        </Security>
+      </Router>
+    );
+  }
 }
 
 export default App;
